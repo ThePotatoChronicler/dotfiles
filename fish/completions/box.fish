c1ac44e3e4a91363944441a99a5b9fd08ce914ae
Potato Chronicler <potater@potater.dev>
2025-11-04 18:10:28 +0100

    docs[fish]: box no-create flag

--- fish/completions/box.fish
+++ fish/completions/box.fish
@@ -1,2 +1,3 @@
 complete -c box -l variant -r -a "base javascript" -d "Image variant"
 complete -c box -l mount-cwd -d "Mounts current working directory"
+complete -c box -l no-create -d "Forbids creation of new boxes"
